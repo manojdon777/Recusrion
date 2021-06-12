02765a3129a75855a7476afba6b98602962e1ae5
manojdon777 <manojdon777@gmail.com>
2021-06-12 13:25:50 +0530

    Update Sort_Array_Recursion.py

--- Sort_Array_Recursion.py
+++ Sort_Array_Recursion.py
@@ -1,26 +1,23 @@
+arr = [0,1,5,2]
 arr = [2, 3, 5, 4, 10, 3, 4, 5, 2, 1, -10, 0]
 class Sorting:
+    def insert(self, arr, num):
+        length = len(arr)
+        if len(arr) == 0 or arr[ len(arr)-1 ] <= num:
+            arr.append(num)
+            return   
+        temp = arr.pop()
+        self.insert(arr, num)
+        arr.append(temp)
 
-    def insert(self, arr, temp):
-        if len(arr) == 0 or arr[len(arr) - 1] <= temp:
-            arr.append(temp)
-            return
-        
-        val = arr[len(arr) - 1]
-        arr.pop()
-        self.insert(arr, temp)
-        arr.append(val)
-        
-
-    def sort(self, arr):
+    def sort(self, arr):        
         if len(arr) == 1:
             return
-        temp = arr[len(arr) - 1]
-        arr.pop()
+        temp = arr.pop()
         self.sort(arr)
         self.insert(arr, temp)
         return arr
-    
 
 obj = Sorting()
 print(obj.sort(arr))
+
